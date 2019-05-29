d8025710abdd5deca73e2917f7187f092b32e240
Microoo <Microoo@users.noreply.github.com>
2019-05-30 01:08:29 +0300

    Input managing & debug command

--- index.js
+++ index.js
@@ -1,48 +1,66 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const credentials = require("./credentials.json");
 
 const getRepoInfo = require('git-repo-info');
 const gitInfo = getRepoInfo();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
 
-  if (msg.content === 'uptime') {
-    msg.reply(Math.round(process.uptime()) + " seconds");
-  }
+    // Validate user input for easier handling
+    if (msg.author.bot) return;
+    if (msg.content[0] !== config.prefix) return;
+    var input = msg.content.toLowerCase().slice(1);
 
-  if (msg.content === 'github') {
-    msg.channel.send({embed: {
-      color: 3447003,
-      author: {
-        name: client.user.username,
-        icon_url: client.user.avatarURL
-      },
-      title: "Fewbewki Repo Info",
-      url: "https://github.com/Microoo/fewbewki",
-      description: "Cool description :).",
-      fields: [{
-        name: "General",
-        value: "Branch: " + gitInfo.branch,
-      },
-      {
-        name: "Last Commit",
-        value: gitInfo.committer + " at " + gitInfo.committerDate + " _'" + gitInfo.commitMessage + "'_"
-      },
-      ],
-      timestamp: new Date(),
-      footer: {
-      icon_url: client.user.avatarURL,
-      text: "Fewbewki"
-      }
-      }
-    });
-  }
+    // Dump variables etc
+    if (input === 'debug') {
+        // Return if not Mico or Kaiku user ID
+        console.log(msg.author.id);
+        if (msg.author.id != "136856906139566081" && msg.author.id != "133701588651999232") return;
+        msg.channel.send("Input: " + msg.content + " |  Validated: " + input);
+    }
+
+    // Uptime and other possible info in the future
+    if (input === 'uptime') {
+        msg.reply(Math.round(process.uptime()) + " seconds");
+    }
+
+    // Github info
+    if (input === 'github') {
+        msg.channel.send({
+            embed: {
+                color: 3447003,
+                author: {
+                    name: client.user.username,
+                    icon_url: client.user.avatarURL
+                },
+                title: "https://github.com/Microoo/fewbewki",
+                url: "https://github.com/Microoo/fewbewki",
+                // IDEA: Add random flavor text from array
+                description: "Cool description here :)",
+                fields: [{
+                        name: "General",
+                        value: "Branch: " + gitInfo.branch,
+                    },
+                    {
+                        name: "Last Commit",
+                        value: gitInfo.committer + " at " + gitInfo.committerDate + " _'" + gitInfo.commitMessage + "'_"
+                    },
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: client.user.avatarURL,
+                    text: "Fewbewki"
+                }
+            }
+        });
+    }
 
 });
 
-client.login(config.token);
+client.login(credentials.token);
